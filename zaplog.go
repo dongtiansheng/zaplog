6a4e3a1a001f00c408e28acadf943523d1fabbe5
dts <m176451143779@163.com>
2020-07-18 10:03:16 +0800

    用于gin的zap日志组件

---
+++ zaplog.go
@@ -0,0 +1,164 @@
+package log
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"github.com/gin-gonic/gin"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"runtime/debug"
+	"strings"
+	"time"
+)
+
+var (
+	Logger *zap.Logger
+)
+
+type LogConfig struct {
+	LogPath      string // logPath 日志文件路径
+	LogLevel     string // logLevel 日志级别 debug/info/warn/error
+	MaxSize      int    // maxSize 单个文件大小,MB
+	MaxBackups   int    // maxBackups 保存的文件个数
+	MaxAge       int    // maxAge 保存的天数， 没有的话不删除
+	Compress     bool   // compress 压缩
+	JsonFormat   bool   // jsonFormat 是否输出为json格式
+	ShowLine     bool   // shoowLine 显示代码行
+	LogInConsole bool   // logInConsole 是否同时输出到控制台
+}
+
+func InitLogger(cfg LogConfig) {
+	hook := lumberjack.Logger{
+		Filename:   cfg.LogPath,    // 日志文件路径
+		MaxSize:    cfg.MaxSize,    // megabytes
+		MaxBackups: cfg.MaxBackups, // 最多保留300个备份
+		Compress:   cfg.Compress,   // 是否压缩 disabled by default
+	}
+	if cfg.MaxAge > 0 {
+		hook.MaxAge = cfg.MaxAge // days
+	}
+
+	var syncer zapcore.WriteSyncer
+	if cfg.LogInConsole {
+		syncer = zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook))
+	} else {
+		syncer = zapcore.AddSync(&hook)
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "line",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.ShortCallerEncoder,     // 全路径编码器
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	var encoder zapcore.Encoder
+	if cfg.JsonFormat {
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	} else {
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
+	}
+
+	// 设置日志级别,debug可以打印出info,debug,warn；info级别可以打印warn，info；warn只能打印warn
+	// debug->info->warn->error
+	var level zapcore.Level
+	switch cfg.LogLevel {
+	case "debug":
+		level = zap.DebugLevel
+	case "info":
+		level = zap.InfoLevel
+	case "warn":
+		level = zap.WarnLevel
+	case "error":
+		level = zap.ErrorLevel
+	default:
+		level = zap.InfoLevel
+	}
+
+	core := zapcore.NewCore(
+		encoder,
+		syncer,
+		level,
+	)
+	Logger = zap.New(core)
+	if cfg.ShowLine {
+		Logger = Logger.WithOptions(zap.AddCaller())
+	}
+}
+
+// GinLogger 接收gin框架默认的日志
+func GinLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+		c.Next()
+
+		cost := time.Since(start)
+		logger.Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("path", path),
+			zap.String("query", query),
+			zap.String("ip", c.ClientIP()),
+			zap.String("user-agent", c.Request.UserAgent()),
+			zap.String("errors", c.Errors.ByType(gin.ErrorTypePrivate).String()),
+			zap.Duration("cost", cost),
+		)
+	}
+}
+
+// GinRecovery recover掉项目可能出现的panic，并使用zap记录相关日志
+func GinRecovery(logger *zap.Logger, stack bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					logger.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					c.Error(err.(error))
+					c.Abort()
+					return
+				}
+
+				if stack {
+					logger.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("stack", string(debug.Stack())),
+					)
+				} else {
+					logger.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+				}
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		}()
+		c.Next()
+	}
+}
